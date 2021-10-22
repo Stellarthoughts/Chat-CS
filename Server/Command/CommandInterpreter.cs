4cc5ac3d0d7672e6746ea7041adc7218c75d4ebd
Pavel Astral <astrasourcenext@gmail.com>
2021-10-22 12:05:51 +0700

    add: CommandInterpreter

---
+++ Server/Command/CommandInterpreter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Server.CommandPM
+{
+	public class CommandInterpreter
+	{
+		private List<Command> commandList;
+
+		public CommandInterpreter(List<Command> commandList)
+        {
+			this.commandList = commandList;
+        }
+
+		public Command InterpretMessage(ChatPM.Message msg)
+		{
+            foreach(Command command in commandList)
+            {
+				if(command.Validate(msg) == -1) continue;
+				if(command.Recognize(msg) == -1) continue;
+				return command;
+            }
+
+			return new UnrecognizedCommand();
+		}
+	}
+}
---
+++ Server/Command/ICommand.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System;
+using Server.ChatPM;
+
+namespace Server.CommandPM
+{
+	public abstract class Command
+	{
+		protected List<string> keywords;
+		protected string regulatorSymbol;
+		protected bool hasArgs;
+
+		protected List<string> arguments;
+		public void setArguments(List<string> arguments) => this.arguments = arguments;
+
+		public Command()
+		{
+			keywords = new List<string>();
+		}
+
+		public int Validate(Message msg)
+        {
+			if (!msg.Content.Contains(RegulatorSymbol)) return -1;
+			foreach (string keyword in Keywords)
+			{
+				if (!msg.Content.Contains(keyword)) return -1;
+			}
+			return 0;
+		}
+
+		public abstract int Recognize(Message msg);
+		public abstract void Execute();
+
+		public List<string> Keywords => keywords;
+		public string RegulatorSymbol => regulatorSymbol;
+	}
+
+
+	public class UnrecognizedCommand : Command
+	{ 
+		public UnrecognizedCommand() : base()
+        {
+
+        }
+		public override void Execute()
+		{
+
+		}
+		public override int Recognize(Message msg)
+		{
+			return -1;
+		}
+	}
+
+	public class ConnectCommand : Command
+    {
+		public ConnectCommand() : base()
+        {
+            keywords.Add("connect");
+			hasArgs = true;
+        }
+		public override void Execute()
+		{
+			if(hasArgs == true && arguments.Count == 0) throw new Exception();
+			
+		}
+		public override int Recognize(Message msg)
+        {
+			return -1;
+        }
+	}
+}
