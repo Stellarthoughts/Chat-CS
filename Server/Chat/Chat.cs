c38c717ef40e49f7316c817b022c74a8f14f3e1e
Pavel Astral <astrasourcenext@gmail.com>
2021-10-22 12:05:36 +0700

    add: Chat

---
+++ Server/Chat/Chat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Server.CommandPM;
+
+namespace Server.ChatPM
+{
+    public class Chat
+    {
+        protected Server srv;
+        protected int id;
+
+        protected List<ChatEntity> connected;
+        protected List<Message> savedMessages;
+        protected CommandInterpreter commandInterpeter;
+        
+        
+        public Chat(Server srv)
+        {
+            this.srv = srv;
+            id = Server.IdGlobal;
+            connected = new List<ChatEntity>();
+            savedMessages = new List<Message>();
+            commandInterpeter = new CommandInterpreter(
+                new List<Command>()
+                );
+
+        }
+
+        public void ReceiveMessage(Message msg)
+        {
+            NewMessage(msg);
+        }
+
+        public void NewMessage(Message msg)
+        {
+            savedMessages.Add(msg);
+        }
+
+        public int Connect(ChatEntity entity)
+        {
+            entity.ConnectedTo = this;
+            connected.Add(entity);
+            return 0;
+        }
+
+        public int Disconnect(ChatEntity entity)
+        {
+            entity.ConnectedTo = null;
+            connected.Remove(entity);
+            return 0;
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using Server.ChatPM;
+using System;
+using System.Collections.Generic;
 
 namespace Server
 {
-    class Program
-    {
-        static void Main(string[] args)
+	public class Server
+	{
+		private static int idGlobal = 0;
+		private List<Chat> chatList;
+		private List<User> userList;
+
+		private static void Main(string[] args)
+		{
+			Server srv = new Server();
+			Chat mainchat = new Chat(srv);
+			User localUser = new User(srv);
+
+			while(true)
+			{
+				try
+				{
+					localUser.UI.WaitForInput();
+				}
+				catch(Exception ex)
+				{
+					break;
+				}
+			}
+		}
+
+		public void ConnectUserToServer(User usr)
         {
-            Console.WriteLine("Hello World!");
+			if(!userList.Contains(usr))
+            {
+				userList.Add(usr);
+            }
+			else
+            {
+				throw new Exception();
+            }
         }
-    }
+
+		public void AddChat(Chat chat)
+        {
+			if(!chatList.Contains(chat))
+            {
+				chatList.Add(chat);
+            }
+			else
+            {
+				throw new Exception();
+            }
+        }
+
+		public void ReceiveMessage(Message msg)
+        {
+
+        }
+
+		private void ConnectUserToChat(User usr, Chat cht)
+		{
+
+		}
+
+		public static int IdGlobal { get => idGlobal++; }
+		public static int IdGlobalCheckout { get => idGlobal; }
+	}
 }
