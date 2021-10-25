29f6231be8c6eaf714e64d1dfc04b7769f1abbe7
Pavel Astral <astrasourcenext@gmail.com>
2021-10-25 19:29:23 +0700

    add: Bot

--- Server/Bot/Bot.cs
+++ Server/Bot/Bot.cs
@@ -1,12 +1,32 @@
 ﻿using Server.ChatPM;
+using Server.CommandPM;
+using System.Collections.Generic;
 
 namespace Server.BotPM
 {
 	public class Bot : ChatEntity
 	{
-		public Bot()
-		{
+        protected CommandInterpreter commandInterpeter;
 
-		}
-	}
+        public Bot()
+		{
+            commandInterpeter = new CommandInterpreter(
+               this,
+               new List<Command> {
+                    new UnrecognizedCommand(),
+                    new HelloCommand()
+                   }
+           );
+        }
+
+        public override void ReceiveMessage(Message msg)
+        {
+            Command command = commandInterpeter.InterpretMessage(msg);
+            command.Execute();
+        }
+        public override void SendMessage(Message msg)
+        {
+            connectedTo.ReceiveMessage(msg);
+        }
+    }
 }
