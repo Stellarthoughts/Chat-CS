ec860ee7b679a7f282a6fb11de060cfaee5118e9
Stellarthoughts <49097167+Stellarthoughts@users.noreply.github.com>
2022-03-22 12:26:19 +0700

    tests

---
+++ Chat.Tests/ConnectionManagerUnitTests.cs
@@ -0,0 +1,35 @@
+using Chat.Server.SocketsManager;
+using Moq;
+using NUnit.Framework;
+using System.Net.WebSockets;
+
+namespace Chat.Tests
+{
+    public class Tests
+    {
+        private Mock<WebSocket>? _socket = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            _socket = new Mock<WebSocket>(); 
+        }
+
+        [Test]
+        public void Test_Socket_Adds_Correctly()
+        {
+            ConnectionManager con = new ConnectionManager();
+            con.AddSocket(_socket.Object);
+            Assert.IsNotEmpty(con.GetAllConnections());
+        }
+
+        [Test]
+        public void Test_ConnectionManager_Can_Get_ID()
+        {
+            ConnectionManager con = new ConnectionManager();
+            con.AddSocket(_socket.Object);
+            var id = con.GetId(_socket.Object);
+            Assert.IsNotNull(id);
+        }
+    }
+}
